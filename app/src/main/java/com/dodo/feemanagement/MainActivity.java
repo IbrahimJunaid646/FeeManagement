1ab4ac21e28a24064576e249c869d10c13272711
IbrahimJunaid646 <ibrahimjunaid646@gmail.com>
2022-12-26 20:32:44 +0500

    Initial commit

---
+++ app/src/main/java/com/dodo/feemanagement/MainActivity.java
@@ -0,0 +1,33 @@
+package com.dodo.feemanagement;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton signInButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        signInButton=findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
